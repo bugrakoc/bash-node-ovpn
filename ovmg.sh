a1b189afab7fcc38df77c088aa2fdb8b7ca878d2
bugrakoc <bugra299@gmail.com>
2018-10-29 00:16:18 +0300

    Update ovmg.sh

--- ovmg.sh
+++ ovmg.sh
@@ -3,3 +3,18 @@
 # https://github.com/bugrakoc/bash-node-ovpn
 #
 # Bash script for node.js - openvpn integration
+
+result () {}
+
+if [[ -e /etc/debian_version ]]; then
+	OS=debian
+	GROUPNAME=nogroup
+	RCLOCAL='/etc/rc.local'
+elif [[ -e /etc/centos-release || -e /etc/redhat-release ]]; then
+	OS=centos
+	GROUPNAME=nobody
+	RCLOCAL='/etc/rc.d/rc.local'
+else
+	echo "This needs to run on Debian or CentOS"
+	exit
+fi
